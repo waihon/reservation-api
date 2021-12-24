5177fa524cfecae4cbf0a3b1c63e4049825aecc4
Waihon Yew <yewwaihon@gmail.com>
2021-12-24 23:32:46 +0800

    Update models with relationship and validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,8 @@
 class Reservation < ApplicationRecord
+  belongs_to :guest
+
+  validates :code, uniqueness: { case_sensitive: false }
+  validates_presence_of :code, :start_date, :end_date, :nights, :guests,
+    :adults, :children, :infants, :status, :currency, :payout_price,
+    :security_price, :total_price
 end
