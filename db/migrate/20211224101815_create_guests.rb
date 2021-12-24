ef333e5bc120d31f178878aaa8ffa5254af04156
Waihon Yew <yewwaihon@gmail.com>
2021-12-24 22:28:19 +0800

    Update guests migration with fields and run it

--- db/migrate/20211224101815_create_guests.rb
+++ db/migrate/20211224101815_create_guests.rb
@@ -1,6 +1,12 @@
 class CreateGuests < ActiveRecord::Migration[6.1]
   def change
     create_table :guests do |t|
+      t.string :email
+      t.string :first_name
+      t.string :last_name
+      t.string :phone_1
+      t.string :phone_2
+      t.string :phone_3
 
       t.timestamps
     end
