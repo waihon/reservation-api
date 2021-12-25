3a62a2e5f519556d93729942cd7d3075fb89328e
Waihon Yew <yewwaihon@gmail.com>
2021-12-25 17:32:55 +0800

    Fix a typo in guest model

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,5 +1,5 @@
 class Guest < ApplicationRecord
-  has_many :reservations, dependent :destroy
+  has_many :reservations, dependent: :destroy
 
   validates :email, uniqueness: { case_sensitive: false }
   validates_presence_of :email, :first_name, :last_name, :phone
