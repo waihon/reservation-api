bfdbfa229cc10a60f89c4bef5d4e88a384378150
Waihon Yew <yewwaihon@gmail.com>
2021-12-26 12:04:18 +0800

    Parse payload #2 by subclassing and customizing the base parser

--- app/services/reservation_parser.rb
+++ app/services/reservation_parser.rb
@@ -21,7 +21,8 @@ class ReservationParser
 
   def self.inherited(candidate)
     # Subclasses that inherit from this base parser will be added to
-    # the registry.
+    # the registry. Such subclassess will somehow have to be in the same
+    # source file as the base class for the inherited hook to work.
     register(candidate)
   end
 
@@ -32,10 +33,11 @@ class ReservationParser
     !!params.dig(:reservation_code) && !!params.dig(:email)
   end
 
-  def self.reservation_variables
+  def reservation_variables
     # This parser uses instance variables with the same name as that
     # of the payload.
-    # This method may not need to be overriden for an alternative parser.
+    # This method may need to be overriden by a child parser for
+    # additional fields and/or fields not found in the base parser.
     [
       "reservation_code",
       "start_date",
@@ -57,12 +59,12 @@ class ReservationParser
     ]
   end
 
-  def self.reservation_params
+  def reservation_params
     # This parser uses parameters with the same name as that of the
     # instance variables.
-    # This method may need to be overriden for an alternative parser
+    # This method may need to be overriden by a child parser for payloads
     # with different parameters.
-    self.reservation_variables
+    reservation_variables
   end
 
   def initialize(params)
@@ -72,42 +74,62 @@ class ReservationParser
   def map_params_to_variables
     # This method assigns param values to normalized instance variables
     # which will be used in downstream parsing.
-    self.class.reservation_variables.zip(self.class.reservation_params) do |variable, param|
+    reservation_variables.zip(reservation_params) do |variable, param|
       # E.g. @reservation_code = params.dig("reservation_code")
       instance_variable_set("@" + variable, params.dig(param))
     end
   end
 
   def parse
-    # This method contains the main parsing logic and may not need to be
-    # overriden for an alternative parser.
+    # This method contains the main parsing logic and uses the template
+    # design pattern.
+    # It may not need to be overriden by a child parser.
     map_params_to_variables
 
+    # Guest email field should be unique based on the requirement.
+    # When an email is not found, we create a new guest object, eelse
+    # to update an existing one.
+    # @email is populated in the map_params_to_variables method.
     @guest = Guest.find_by(email: @email) || Guest.new
     update_guest
 
+    # Reservation code field should be unique based on the requirement.
+    # When a reservation code is not found, we cerate a new reservation
+    # object, elease to update an existing one.
+    # @reservation_code is populated in the map_params_to_variables method.
     @reservation = Reservation.find_by(reservation_code: @reservation_code) || Reservation.new
     update_reservation
 
+    # Return the populated guest and reservation model objects to the caller
+    # for further processing.
     return @guest, @reservation
   end
 
   def update_guest
-    # This method contains the parsing logic for a guest and may need to be
-    # overriden for an alternative parser.
+    # This method contains the parsing logic for the common fields of a guest
+    # and may not need to be overriden by a child parser.
     begin
       guest.email = @email if @email.present?
       guest.first_name = @first_name if @first_name.present?
       guest.last_name = @last_name if @last_name.present?
-      guest.phone = @phone if @phone.present?
+
+      # If there's any guest fields specific to a payload, we should include
+      # them in below hook.
+      update_additional_guest_fields
     rescue => exception
-      raise exception  
+      raise exception
     end
   end
 
+  def update_additional_guest_fields
+    # This method is for updating additional guest fields specific to a
+    # payload format and needs to be overriden.
+    guest.phone_1 = @phone if @phone.present?
+  end
+
   def update_reservation
-    # This method contains the parsing logic for a reservation and may need
-    # to be overriden for an alternative parser.
+    # This method contains the parsing logic for the common fields of a
+    # reservation and may not need to be overriden by a child parser.
     begin
       reservation.reservation_code = @reservation_code if @reservation_code.present?
       reservation.start_date = Date.parse(@start_date) if @start_date.present?
@@ -115,7 +137,7 @@ class ReservationParser
       reservation.nights = Integer(@nights) if @nights.present?
       reservation.guests = Integer(@guests) if @guests.present?
       reservation.adults = Integer(@adults) if @adults.present?
-      reservation.children = Integer(@children) if @children.present? 
+      reservation.children = Integer(@children) if @children.present?
       reservation.infants = Integer(@infants) if @infants.present?
       reservation.status = @status if @status.present?
       reservation.currency = @currency if @currency.present?
@@ -123,8 +145,83 @@ class ReservationParser
       reservation.security_price = @security_price.to_f if @security_price.present?
       reservation.total_price = @total_price.to_f if @total_price.present?
       reservation.guest = guest
+
+      # If there's any reservation fields specific to a payload, we should
+      # include them in below hook.
+      update_additional_reservation_fields
     rescue => exception
-      raise exception  
+      raise exception
+    end
+  end
+
+  def update_additional_reservation_fields
+    # This method is for updating additional reservation fields specific to
+    # a payload format and needs to be overriden by a child parser.
+    # There's no additional reservation fields for payload #1.
+  end
+end
+
+# We'll need to include the subclasses in the same source file as the base
+# class in order for the inherited hook to work (based on observation).
+class ReservationParser2 < ReservationParser
+  def self.handles?(params)
+    # This parser handles payload #2 which includes code and guest_email.
+    # !! are used to convert to logical expressions.
+    !!params.dig(:code) && !!params.dig(:guest_email)
+  end
+
+  def reservation_variables
+    # Payload #2 doesn't have phone but has 2 other fields not found in
+    # payout #1.
+    super - ["phone"] + ["phones", "localized_description"]
+  end
+
+  def reservation_params
+    # This parser has enough parameters with differnt name from that of the
+    # instance variables to warrant a separate declaration.
+    [
+      "code",
+      "start_date",
+      "end_date",
+      "nights",
+      "number_of_guests",
+      "number_of_adults",
+      "number_of_children",
+      "number_of_infants",
+      "status_type",
+      "host_currency",
+      "expected_payout_amount",
+      "listing_security_price_accurate",
+      "total_paid_amount_accurate",
+      "guest_email",
+      "guest_first_name",
+      "guest_last_name",
+      "guest_phone_numbers",
+      "localized_description"
+    ]
+  end
+
+  def update_additional_guest_fields
+    # This method is for updating additional guest fields specific to
+    # payload #2.
+    begin
+      @phones.take(3).each_with_index do |phone, index|
+        # Populate phone_1, phone_2, and/or phone_3.
+        # The index is zero-based so needs to increment by 1.
+        guest.send("phone_#{index + 1}=", phone)
+      end
+    rescue => exception
+      raise exception
+    end
+  end
+
+  def update_additional_reservation_fields
+    # This method is for updating additional reservation fields specific to
+    # payload #2.
+    begin
+      reservation.localized_description = @localized_description if @localized_description.present?
+    rescue => exception
+      raise exception
     end
   end
 end
