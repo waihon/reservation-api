0ba5f55254ca08c67f54435ad92492881828b2b8
Waihon Yew <yewwaihon@gmail.com>
2021-12-26 11:43:11 +0800

    Rename guest model's phone to be consistent with database changes

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -2,5 +2,5 @@ class Guest < ApplicationRecord
   has_many :reservations, dependent: :destroy
 
   validates :email, uniqueness: { case_sensitive: false }
-  validates_presence_of :email, :first_name, :last_name, :phone
+  validates_presence_of :email, :first_name, :last_name, :phone_1
 end
